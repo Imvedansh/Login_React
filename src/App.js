91b2bf913e5d725dda9d3aa61a003b37ecec4b4b
Imvedansh <113465074+Imvedansh@users.noreply.github.com>
2023-09-08 17:54:26 +0530

    Initialize project using Create React App
    Form_Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Login_Page from './Content/Login_Page/Login_Page';
+
+function App() {
+  return (
+    <div>
+      <Login_Page/>
+    </div>
+  );
+}
+
+export default App;
